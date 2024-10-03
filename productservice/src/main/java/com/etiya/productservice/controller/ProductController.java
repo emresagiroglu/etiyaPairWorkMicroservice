c3e05768bcd267fd5f7bf5bd0a23f7245521ea2d
Emre Sağıroğlu (ETIYA) <emre.sagiroglu@etiya.com>
2024-10-03 15:49:32 +0300

    gateway service added

---
+++ productservice/src/main/java/com/etiya/productservice/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.etiya.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping()
+    public String hello(){
+        return "Hi from Product Service";
+    }
+}
