2e8faf13f96bc1e8f03f49ddd768442211497494
Aslı Sema Gültekin (ETIYA) <asli.gultekin@etiya.com>
2024-10-14 20:30:52 +0300

    create dtos for ProductSpec and Specification entities

---
+++ catalogservice/src/main/java/com/etiya/catalogservice/dtos/productSpec/CreatedProductSpecResponseDto.java
@@ -0,0 +1,19 @@
+package com.etiya.catalogservice.dtos.productSpec;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatedProductSpecResponseDto {
+
+    private String value;
+
+    private UUID specificationId;
+}
