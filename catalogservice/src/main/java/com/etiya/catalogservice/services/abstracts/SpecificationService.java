ed894e8ed888852d328f7064073028acf8b6f2bc
Aslı Sema Gültekin (ETIYA) <asli.gultekin@etiya.com>
2024-10-16 22:28:38 +0300

    add service and controller for specification

---
+++ catalogservice/src/main/java/com/etiya/catalogservice/services/abstracts/SpecificationService.java
@@ -0,0 +1,21 @@
+package com.etiya.catalogservice.services.abstracts;
+
+import com.etiya.catalogservice.dtos.category.*;
+import com.etiya.catalogservice.dtos.specification.*;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SpecificationService {
+
+    CreatedSpecificationResponseDto add(CreateSpecificationRequestDto createSpecificationRequestDto);
+
+    GetSpecificationResponseDto getById(UUID id);
+
+    List<ListSpecificationResponseDto> getAll();
+
+    UpdatedSpecificationResponseDto update(UUID id, UpdateSpecificationRequestDto updateSpecificationRequestDto);
+
+    void delete(UUID id);
+
+}
