4c8f2351a83f5337dc901c73c5448316e79ceeba
emre.sagiroglu <emresagiroglu1@gmail.com>
2024-10-16 14:26:43 +0300

    identity deleted

--- identityservice/src/main/java/com/etiya/identityservice/service/auth/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.etiya.identityservice.service.auth;
-
-
-import com.etiya.identityservice.dto.LoginRequestDto;
-
-public interface AuthService {
-
-    String login(LoginRequestDto loginRequest);
-}
--- identityservice/src/main/java/com/etiya/identityservice/service/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.etiya.identityservice.service.user;
-
-import com.etiya.identityservice.dto.UserAddRequestDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    void add(UserAddRequestDto request);
-
-}
