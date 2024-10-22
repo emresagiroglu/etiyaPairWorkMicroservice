96b2bd34d1149cd1931f725bbad5df0fab9c6814
emre.sagiroglu <emresagiroglu1@gmail.com>
2024-10-22 17:23:09 +0300

    product service removed

--- productservice/src/main/java/com/etiya/productservice/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etiya.productservice.repositories;
-
-import com.etiya.productservice.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
--- productservice/src/main/java/com/etiya/productservice/services/ProductService.java
+++
@@ -1,4 +0,0 @@
-package com.etiya.productservice.services;
-
-public interface ProductService {
-}
